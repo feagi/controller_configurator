ebf673912c4d401f12096a415b44be686b36e5b6
Amir Rasteg <amir.rasteg@protonmail.com>
2024-08-19 20:42:18 -0400

    dont export invisible parameters

--- UI_Components/Device/Device/Device.gd
+++ UI_Components/Device/Device/Device.gd
@@ -21,6 +21,8 @@ func export_as_dict() -> Dictionary:
 	var details: Dictionary = {}
 	for i in range(1, holder.get_child_count()):
 		var parameter: BaseParameter = holder.get_child(i)
+		if !parameter.visible:
+			continue # dont add invisible parameters
 		details.merge(parameter.get_value_as_dict())
 	return {get_physical_ID() : details}
 		

ff5084eee9b6f000689d8d35924f835f2ae62574
Amir Rasteg <amir.rasteg@protonmail.com>
2025-02-05 17:44:50 -0500

    NARRATOR: (Black screen with text; The sound of buzzing bees can be heard) According to all known laws of aviation,  : there is no way a bee should be able to fly.  : Its wings are too small to get its fat little body off the ground.  : The bee, of course, flies anyway  : because bees don't care what humans think is impossible. BARRY BENSON: (Barry is picking out a shirt) Yellow, black. Yellow, black. Yellow, black. Yellow, black.  : Ooh, black and yellow! Let's shake it up a little. JANET BENSON: Barry! Breakfast is ready! BARRY: Coming!  : Hang on a second. (Barry uses his antenna like a phone)  : Hello? ADAM FLAYMAN:
    (Through phone)
    - Barry?
    BARRY:
    - Adam?
    ADAM:
    - Can you believe this is happening?
    BARRY:
    - I can't. I'll pick you up.
    (Barry flies down the stairs)
     :
    MARTIN BENSON:
    Looking sharp.
    JANET:
    Use the stairs. Your father
    paid good money for those.
    BARRY:
    Sorry. I'm excited.
    MARTIN:
    Here's the graduate.
    We're very proud of you, son.
     :
    A perfect report card, all B's.
    JANET:
    Very proud.
    (Rubs Barry's hair)
    BARRY=
    Ma! I got a thing going here.
    JANET:
    - You got lint on your fuzz.
    BARRY:
    - Ow! That's me!
    
    JANET:
    - Wave to us! We'll be in row 118,000.
    - Bye!
    (Barry flies out the door)
    JANET:
    Barry, I told you,
    stop flying in the house!
    (Barry drives through the hive,and is waved at by Adam who is reading a
    newspaper)
    BARRY==
    - Hey, Adam.
    ADAM:
    - Hey, Barry.
    (Adam gets in Barry's car)
     :
    - Is that fuzz gel?
    BARRY:
    - A little. Special day, graduation.
    ADAM:
    Never thought I'd make it.
    (Barry pulls away from the house and continues driving)
    BARRY:
    Three days grade school,
    three days high school...
    ADAM:
    Those were awkward.
    BARRY:
    Three days college. I'm glad I took
    a day and hitchhiked around the hive.
    ADAM==
    You did come back different.
    (Barry and Adam pass by Artie, who is jogging)
    ARTIE:
    - Hi, Barry!
    
    BARRY:
    - Artie, growing a mustache? Looks good.
    ADAM:
    - Hear about Frankie?
    BARRY:
    - Yeah.
    ADAM==
    - You going to the funeral?
    BARRY:
    - No, I'm not going to his funeral.
     :
    Everybody knows,
    sting someone, you die.
     :
    Don't waste it on a squirrel.
    Such a hothead.

--- Steps/Importers/Shared/TreeSkeletonExplorer/TreeSkeletonExplorer.gd
+++ Steps/Importers/Shared/TreeSkeletonExplorer/TreeSkeletonExplorer.gd
@@ -13,7 +13,7 @@ func setup(ref_to_robot_config_template_holder: FEAGIRobotConfigurationTemplateH
 		{
 			"name": str name of the node,
 			"type": str value with options "body", "input", or "output",
-			"subtype": str value that exists if type is input or output. defines the type of device
+			"feagi device type": str value that exists if type is input or output. defines the type of device
 			"properties": dictionary that exists if type is input or output, string keys with the data property with the value being the property
 			"description": optional str description of the node. may not exist or be blank,
 			"children": array with this dictionary structure recursively
@@ -21,13 +21,14 @@ func setup(ref_to_robot_config_template_holder: FEAGIRobotConfigurationTemplateH
 	]
 	"""
 	clear()
-	if len(imported_possible_devices_root_tree_nodes) == 0:
-		push_error("No nodes to import!")
-		return Error.ERR_DOES_NOT_EXIST
-	
 	
 	_robot_config_template_holder_ref = ref_to_robot_config_template_holder
 	
+	if len(imported_possible_devices_root_tree_nodes) == 0:
+		push_error("No nodes to import!")
+		return Error.ERR_DOES_NOT_EXIST
+		
+	# Make sure we have only 1 root node. If not, create one root node to hold all given nodes
 	var imported_possible_devices_tree: Dictionary
 	if len(imported_possible_devices_root_tree_nodes) == 1:
 		imported_possible_devices_tree = imported_possible_devices_root_tree_nodes[0]
@@ -39,15 +40,15 @@ func setup(ref_to_robot_config_template_holder: FEAGIRobotConfigurationTemplateH
 			"children": imported_possible_devices_root_tree_nodes
 		}
 	
+	if imported_possible_devices_tree["type"] != "body": # NOTE: Root node must always be a body
+		push_error("Root body must be of type 'body'!")
+		return Error.ERR_INVALID_DATA
+	
 	var root_node: TreeItem = create_item(null) 
 	var root_node_ok: Error = _check_and_populate_tree_node_UI(root_node, imported_possible_devices_tree)
 
-	
 	if root_node_ok != Error.OK:
 		push_error("Root Node Invalid!")
-		return Error.ERR_INVALID_DATA
-	if imported_possible_devices_tree["type"] != "body": # NOTE: Root node must always be a body
-		push_error("Root body must be of type 'body'!")
 		return Error.ERR_INVALID_DATA
 	
 
@@ -62,20 +63,22 @@ func setup(ref_to_robot_config_template_holder: FEAGIRobotConfigurationTemplateH
 
 ## Returns a tree item if all inputs are valid. Otherwise returns null
 func _generate_node_generate_tree(node_info: Dictionary, parent: TreeItem    , ) -> TreeItem:
-	#TODO theming for type?
+	
+	var node_ok: Error = _confirm_node_details_has_required_keys(node_info)
+	if node_ok != Error.OK:
+		return null
 	
 	var new_node: TreeItem = create_item(parent)
-	var node_ok: Error = _check_and_populate_tree_node_UI(new_node, node_info)
+	node_ok = _generate_metadata_of_node(new_node, node_info)
+	if node_ok != Error.OK:
+		push_error("Failed to FEAGI device details for node '%s'! No device will be suggested!" % node_info["name"])
+		# No need to stop, just make sure that we can handle null [FEAGIDevice] in signals!
+	
+	node_ok = _check_and_populate_tree_node_UI(new_node, node_info)
 	if node_ok != Error.OK:
 		push_error("Failed to load node properties for the UI, skipping this node and its branch!")
 		parent.remove_child(new_node)
 		return null
-	
-	if node_info.get("type") in ["input", "output"]:
-		node_ok = _generate_metadata_of_node(new_node, node_info)
-		if node_ok != Error.OK:
-			push_error("Failed to FEAGI device details for node '%s'! No devices will be suggested!" % node_info["name"])
-			# no need to stop
 
 	if "children" in node_info:
 		var children: Array = node_info["children"]
@@ -83,19 +86,50 @@ func _generate_node_generate_tree(node_info: Dictionary, parent: TreeItem    , )
 			_generate_node_generate_tree(child_data, new_node)
 	
 	return new_node
+
+## Checks if for a given node, all required keys exist
+func _confirm_node_details_has_required_keys(node_details: Dictionary) -> Error:
+	if "name" not in node_details:
+		push_error("Missing 'name' key for node!")
+		return Error.ERR_INVALID_PARAMETER
+	if "type" not in node_details:
+		push_error("Missing 'type' key for node!")
+		return Error.ERR_INVALID_PARAMETER
+	if node_details["type"] not in ["body", "input", "output"]:
+		push_error("Unknown type '%s' for node!" % str(node_details["type"]))
+		return Error.ERR_INVALID_PARAMETER
 	
+	if node_details["type"] in ["input", "output"]:
+		if not node_details.has("feagi device type"):
+			push_error("Missing 'feagi device type' key for IO node!")
+			return Error.ERR_INVALID_PARAMETER
+		
+		if not node_details.has("properties"):
+			push_error("Missing 'properties' key! for IO node")
+			return Error.ERR_INVALID_PARAMETER
+	
+	return Error.OK
+		
+
+## Generates metadata holding the node type and the feagi device configured (if relevant
+func _generate_metadata_of_node(tree_node: TreeItem, node_details: Dictionary) -> Error:
+	#var possible_FEAGI_devices: Array[FEAGIDevice] = _init_possible_feagi_devices_for_node(node_details)
+	var to_return: Error = Error.OK
+	var metadata: Dictionary = {
+		"type" : node_details["type"],
+		}
+	if node_details.get("type") in ["input", "output"]:
+		metadata["device"] = _init_FEAGI_device_for_node(node_details)
+		if metadata["device"] == null:
+			to_return = Error.ERR_INVALID_PARAMETER
+	tree_node.set_metadata(0, metadata)
+	
+	return to_return
+
+
 ## If details are valid, returns given [TreeItem] with the UI elements updated. Otherwise return null
 func _check_and_populate_tree_node_UI(tree_node: TreeItem, details: Dictionary) -> Error:
-	# Checks
-	if "name" not in details:
-		push_error("Missing 'name' key!")
-		return Error.ERR_INVALID_PARAMETER
-	if "type" not in details:
-		push_error("Missing 'type' key!")
-		return Error.ERR_INVALID_PARAMETER
-	if details["type"] not in ["body", "input", "output"]:
-		push_error("Unknown type '%s'!" % str(details["type"]))
-		return Error.ERR_INVALID_PARAMETER
+
 	
 	tree_node.set_text(0, details["name"])
 	if "description" in details:
@@ -105,52 +139,35 @@ func _check_and_populate_tree_node_UI(tree_node: TreeItem, details: Dictionary) 
 	# TODO additional UI, colums? icons? buttons?
 	return Error.OK
 
-## Generates the possible geagi devices for a given node, and the selector index
-func _generate_metadata_of_node(tree_node: TreeItem, node_details: Dictionary) -> Error:
-	var possible_FEAGI_devices: Array[FEAGIDevice] = _init_possible_feagi_devices_for_node(node_details)
-	if len(possible_FEAGI_devices) == 0:
-		# Something went wrong if nothing was found
-		return Error.FAILED
-	
-	var metadata: Dictionary = {
-		"selected device index" : 0,
-		"possible devices": possible_FEAGI_devices
-		}
-	tree_node.set_metadata(0, metadata)
-	return Error.OK
 
 
-## Assuming node details dict is valid, returns an array of all possible [FEAGIDevice]s this node could be
-func _init_possible_feagi_devices_for_node(node_details: Dictionary) -> Array[FEAGIDevice]:
+
+## Assuming node details dict is valid, returns the default [FEAGIDevice] for this node. Otherwise returns null
+func _init_FEAGI_device_for_node(node_details: Dictionary) -> FEAGIDevice:
 	# Where 'node_details' is the generated details about this node
 	
 	if node_details.get("type") not in ["input", "output"]:
 		push_error("non input/output node type cannot have FEAGI Devices!")
-		return []
+		return null
 	
 	var is_input: bool = node_details["type"] == "input"
-	var node_subtype: StringName = node_details["subtype"]
-	var node_properties: Dictionary = node_details["properties"].duplicate(true)
+	if not node_details.has("feagi device type"):
+		push_error("Input/Output node type does not have a default FEAGI device type defined!")
+		return null
+	var node_feagi_type: StringName = node_details["feagi device type"]
 	
-	var possible_matching_types_mappings: Array[Dictionary] = []
+	var FEAGI_device: FEAGIDevice = _robot_config_template_holder_ref.spawn_device_from_template(node_feagi_type, is_input)
+	if !FEAGI_device: # if not valid
+		push_error("Unable to spawn FEAGI Device of type '%s'!" % node_feagi_type)
+		return null
 	
-	var output: Array[FEAGIDevice] = []
-	
-	for possible_matching_type_mapping in possible_matching_types_mappings:
-		var matched_FEAGI_type: StringName = possible_matching_type_mapping["type"]
-		var FEAGI_device: FEAGIDevice = _robot_config_template_holder_ref.spawn_device_from_template(matched_FEAGI_type, is_input)
-		if !FEAGI_device: # if not valid
-			push_error("Unable to spawn FEAGI Device of type '%s'!" % matched_FEAGI_type)
-			continue
-
-		## We have the device with default values, lets fill in what we have
+	if node_details.has("name"):
 		FEAGI_device.label = node_details["name"]
-		if "description" in node_details:
-			FEAGI_device.description = node_details["description"]
-		var node_details_map_to_FEAGI_device_properties: Dictionary = possible_matching_type_mapping["properties"] 
-		var new_parameters: Dictionary = node_details["parameters"]
-		FEAGI_device.overwrite_parameter_values(new_parameters)
-
-		output.append(FEAGI_device)
+	if node_details.has("description"):
+		FEAGI_device.description = node_details["description"]
 	
-	return output
+	if node_details.has("properties"):
+		FEAGI_device.overwrite_parameter_values(node_details["properties"])
+	
+	return FEAGI_device
+	

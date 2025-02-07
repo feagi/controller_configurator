5139b4259091d1667106ff4f5d06e03e76c702c9
Amir Rasteg <amir.rasteg@protonmail.com>
2025-02-06 19:58:04 -0500

    terxt file imtert

--- Steps/SectionStart/SectionStart.gd
+++ Steps/SectionStart/SectionStart.gd
@@ -8,16 +8,28 @@ signal create_from_scratch()
 
 var _import_methods_dropdown: OptionButton
 var _import_file_button: Button
+var _import_text_button: Button
+var _text_import: TextEdit
 
 func _ready() -> void:
 	_import_methods_dropdown = $ImportFrom/CenterContainer/VBoxContainer/HBoxContainer/OptionButton
-	_import_file_button = $ImportFrom/CenterContainer/VBoxContainer/Button
+	_import_file_button = $ImportFrom/CenterContainer/VBoxContainer/HBoxContainer2/FIle
+	_import_text_button = $ImportFrom/CenterContainer/VBoxContainer/HBoxContainer2/Text
+	_text_import = $ImportFrom/CenterContainer/VBoxContainer/TextImport
 	
 	var import_options_folders: PackedStringArray = DirAccess.get_directories_at(IMPORTERS_DIRECTORY)
 	
 	for import_options_folder in import_options_folders:
 		_import_methods_dropdown.add_item(import_options_folder)
 	_import_methods_dropdown.selected = -1
+	_text_import.text = ""
+	_text_import.editable = false
+	
+	
+	#TODO remove this segment once feature is implemented!
+	if OS.get_name() == "Web":
+		_import_file_button.disabled = true
+		_import_file_button.tooltip_text = "File import for web exports not yet available!"
 
 
 func reset_UI() -> void:
@@ -39,38 +51,62 @@ func _option_from_dropdown_selected(index: int) -> void:
 		return
 	_import_file_button.disabled = false
 	_import_file_button.tooltip_text = "Load file to import..."
-	
 
-## Attempts to load the importer by name. If succeeds, emits attempt_import, otherwise logs an error
-func _load_importer_UI() -> void:
+func _open_config_from_file_pressed() -> void:
 	var importer: BaseConfigImporter = _try_to_get_importer_from_dropdown_selection()
 	if !importer:
 		push_error("Unable to load config importer UI!")
 		return
 	
-	if OS.get_name() != "HTML5":
+	if OS.get_name() != "Web":
 		# use this method to get file
 		var file_selector: FileDialog = FileDialog.new()
 		file_selector.use_native_dialog = true
 		file_selector.add_filter("*." + importer.get_expected_file_extension())
 		file_selector.title = "Select your file to import"
 		file_selector.file_mode = FileDialog.FILE_MODE_OPEN_FILE
-		file_selector.file_selected.connect(_non_HTML_filebrowser_picked)
 		file_selector.show()
+		var path: StringName = await file_selector.file_selected
+		
+		var bytes: PackedByteArray = FileAccess.get_file_as_bytes(path)
+		if len(bytes) == 0:
+			push_error("Unable to open the file at path '%s'!" % path)
+			return
+
+		path = path.split("/")[-1] # get filename
+		attempt_import.emit(_try_to_get_importer_from_dropdown_selection(), bytes, path)
+		return
+
 	# TODO HTML5 JS interactions
+	push_error("HTML5 File import not yet Implemented!")
+
+func _open_config_from_text_pressed() -> void:
+	if !_text_import.visible:
+		_text_import.visible = true
+		return
+	
+	if _text_import.text.is_empty():
+		# Don't both trying to import an empty text
+		return
+	
+	var importer: BaseConfigImporter = _try_to_get_importer_from_dropdown_selection()
+	if !importer:
+		push_error("Unable to load config importer UI!")
+		return
+	
+	var bytes: PackedByteArray = _text_import.text.to_utf8_buffer()
+	if len(bytes) == 0:
+		push_error("Unable to parse given string! '%s'!")
+		return
+
+	attempt_import.emit(_try_to_get_importer_from_dropdown_selection(), bytes, "String")
+	return
+
+
 
 
 func _create_scratch_pressed() -> void:
 	create_from_scratch.emit()
-
-func _non_HTML_filebrowser_picked(path: StringName) -> void:
-	var bytes: PackedByteArray = FileAccess.get_file_as_bytes(path)
-	if len(bytes) == 0:
-		push_error("Unable to open the file at path '%s'!" % path)
-		return
-	
-	path = path.split("/")[-1] # get filename
-	attempt_import.emit(_try_to_get_importer_from_dropdown_selection(), bytes, path)
 
 ## Attempts to return the importer UI object as per the drop down. Returns null if something is wrong
 func _try_to_get_importer_from_dropdown_selection() -> BaseConfigImporter:
